3c0f54e0c6b706c1b42de39ea768637aa4b9fb36
Reinette Chatre <reinette.chatre@intel.com>
2021-06-07 09:13:44 -0700

    x86/sgx: Support enclave page permission changes
    In the initial (SGX1) version of SGX, pages in an enclave need to be
    created with permissions that support all usages of the pages, from the
    time the enclave is initialized until it is unloaded. For example,
    pages used by a JIT compiler or when code needs to otherwise be
    relocated need to always have RWX permissions.
    
    SGX2 includes two functions that can be used to modify the enclave page
    permissions of regular enclave pages within an initialized enclave.
    ENCLS[EMODPR] is run from the OS and used to restrict enclave page
    permissions while ENCLU[EMODPE] is run from within the enclave to
    extend enclave page permissions.
    
    Enclave page permission changes need to be approached with care and
    for this reason this initial support is to allow enclave page
    permission changes _only_ if the new permissions are the same or
    more restrictive that the permissions originally vetted at the time the
    pages were added to the enclave. Support for extending enclave page
    permissions beyond what was originally vetted is deferred.
    
    Whether enclave page permissions are restricted or extended it
    is necessary to ensure that the page table entries and enclave page
    permissions are in sync. Introduce a new ioctl, SGX_IOC_PAGE_MODP, to
    support enclave page permission changes. Since the OS has no insight
    in how permissions may have been extended from within the enclave all
    page permission requests are treated as permission restrictions. This
    ioctl is used when enclave page permissions need to be restricted via
    the OS as well as after enclave page permissions have been extended
    from within the enclave (to ensure correct page table entries are
    generated). With this ioctl the user specifies a page range and the
    permissions to be applied to all pages in the provided range. The ioctl
    itself can return an error code based on failures encountered by the OS.
    It is also possible for SGX specific failures to be encountered. Add a
    result output parameter to communicate the SGX return code. It is
    possible for the permission change request to fail on a particular
    page. To support partial success the ioctl will return the number
    of pages that were successfully changed.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- arch/x86/include/uapi/asm/sgx.h
+++ arch/x86/include/uapi/asm/sgx.h
@@ -27,6 +27,8 @@ enum sgx_page_flags {
 	_IOW(SGX_MAGIC, 0x02, struct sgx_enclave_init)
 #define SGX_IOC_ENCLAVE_PROVISION \
 	_IOW(SGX_MAGIC, 0x03, struct sgx_enclave_provision)
+#define SGX_IOC_PAGE_MODP \
+	_IOWR(SGX_MAGIC, 0x04, struct sgx_page_modp)
 
 /**
  * struct sgx_enclave_create - parameter structure for the
@@ -72,6 +74,24 @@ struct sgx_enclave_init {
  */
 struct sgx_enclave_provision {
 	__u64 fd;
+};
+
+/**
+ * struct sgx_page_modp - parameter structure for the %SGX_IOC_PAGE_MODP ioctl
+ * @offset:	starting page offset (page aligned relative to enclave base
+ *		address defined in SECS)
+ * @length:	length of memory (multiple of the page size)
+ * @prot:	new protection bits of pages in range described by @offset
+ *		and @length
+ * @result:	SGX result code of ENCLS[EMODPR] function
+ * @count:	bytes successfully changed (multiple of page size)
+ */
+struct sgx_page_modp {
+	__u64 offset;
+	__u64 length;
+	__u64 prot;
+	__u64 result;
+	__u64 count;
 };
 
 struct sgx_enclave_run;
